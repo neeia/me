99dbe6f078c8f7a7aed31249e179d0ca91fbee78
Neia <neeia2718@gmail.com>
2023-01-09 04:31:14 -0800

    Update metadata

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Neia Hu';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Web Developer, looking for work.';
